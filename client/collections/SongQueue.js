8daa1df2bde4e97951d08d179f648371bfb7e824
Fliko <baxter.flw@gmail.com>
2017-04-05 18:55:41 -0500

    Change Computers

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,32 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
-
   model: SongModel,
 
-  initialize: function() {
+  initialize: function(arg) {
+    this.set('color', 'blue');
+    console.log(this);
+    if (arg) {
+      SongQueue.Collection.create(arg);
+    }
+  },
+
+  playFirst: function() {
+    //return _.first(this.models);
+  },
+
+  add: function(songData) {
+   // console.log(this);
+    SongQueue.create(songData);
+
+    if (this.models.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  at: function(index) {
+  },
+
+  remove: function (song) {
   }
 
 });
